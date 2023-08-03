e8952739af42681d426687b66595cae0def21a25
Horseee <maxinyin@u.nus.edu>
2023-08-03 22:02:01 +0800

    Update evaluate.sh

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -12,5 +12,5 @@ do
     mv $tune_ckpt_name/checkpoint-$epoch/pytorch_model.bin $tune_ckpt_name/checkpoint-$epoch/adapter_model.bin
 
     tune_id="${tune_ckpt_name##*/}"
-    python lm-evaluation-harness/main.py --model hf-causal-experimental --model_args checkpoint=$prune_ckpt/pytorch_model.bin,peft=$tune_ckpt_name/checkpoint-$epoch,config_pretrained=$base_model --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq --device cuda:0 --output_path results/llama13B_${tune_id}_$epoch.json --no_cache
+    python lm-evaluation-harness/main.py --model hf-causal-experimental --model_args checkpoint=$prune_ckpt/pytorch_model.bin,peft=$tune_ckpt_name/checkpoint-$epoch,config_pretrained=$base_model --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq --device cuda:0 --output_path results/${tune_id}_$epoch.json --no_cache
 done
